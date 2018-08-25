fc98ebae176ebd80ec8cd301322da6904cbb4524
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-25 15:28:26 -0600

    Make empty methods. Write populate

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -1,3 +1,23 @@
 class CompleteMe
 
+  def insert(word)
+    #Traverse down the trie, making new nodes as needed
+  end
+
+  def count
+
+  end
+
+  def populate(dictionary)
+    #Call insert over each newline of the dictionary
+    File.readlines(dictionary).each do |line|
+      insert(line)
+    end
+  end
+
+  def suggest(word)
+    #Travese down the trie to the given word/prefix. Collect and return a list of the
+    #closest nodes
+  end
+
 end
