65c8b13af0ae37900e8f41aa06599fbf85251983
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-26 12:23:23 -0600

    Add tests for absent words, word counting

--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/complete_me'
+require './lib/node'
 
 class CompleteMeTest < Minitest::Test
   def test_it_exists
@@ -9,12 +10,35 @@ class CompleteMeTest < Minitest::Test
     assert_instance_of CompleteMe, completion
   end
 
-  def test_it_can_store_words
-    skip
+  def test_last_node_of_new_word
+    completion = CompleteMe.new
+    last_node = completion.insert("pizza")
+    assert_equal "pizza", last_node.word
+    assert_equal "a", last_node.value
+  end
+
+  def test_it_can_detect_if_word_is_present
     completion = CompleteMe.new
     completion.insert("pizza")
-    
+    assert completion.include?("pizza")
+  end
+
+  def test_detect_if_word_is_absent
+    completion = CompleteMe.new
+    completion.insert("pizzaria")
+    refute completion.include?("pizza")
+    refute completion.include?("4i2364876")
+  end
+
+  def test_it_can_count_words
+    completion = CompleteMe.new
+    assert_equal 0, completion.count
+    completion.insert("pizza")
     assert_equal 1, completion.count
+
+    completion.insert("dog")
+    completion.insert("cat")
+    assert_equal 3, completion.count
   end
 
   def test_it_can_suggest_words
