c6efc40b4ef5ba5c696acd7171c7a6e83eaddca1
Preston Jarnagin <prstnjarnagin@gmail.com>
2018-08-28 14:22:21 -0600

    CompleteMe test refactor

--- lib/complete_me.rb
+++ lib/complete_me.rb
@@ -18,10 +18,10 @@ class CompleteMe
     node
   end
 
-  def normalize(word)
-    word.downcase!
-    word.tr(".", "")
-  end
+  # def normalize(word)
+  #   word.downcase!
+  #   word.tr(".", "")
+  # end
 
   def terminal_node(word, node = @root)
     word.chars.each do |char|
@@ -53,15 +53,13 @@ class CompleteMe
     end
   end
 
-  # Takes in a portion of a word as 'snippit'
-  # Returns an array of words that start with 'snippit' sorted by their weights for the given 'snippit'
-  def suggest(snippit)
-    node = terminal_node(snippit)
-    suggestions = node.descendant_words(snippit)
-    return sort_suggestions(suggestions)
-  end
+  # def suggest_from_beginning(snippit)
+  #   node = terminal_node(snippit)
+  #   suggestions = node.descendant_words(snippit)
+  #   return sort_suggestions(suggestions)
+  # end
 
-  def suggest_all(snippit)
+  def suggest(snippit)
     first_char = snippit[0]
     remaining_snippit = snippit.slice(1..-1)
     nodes = all_nodes_by_value(first_char)
@@ -85,7 +83,7 @@ class CompleteMe
   end
 
   def lexicon
-    @root.descendant_words
+    @root.descendant_words.keys
   end
 
   def include?(word)
--- test/complete_me_test.rb
+++ test/complete_me_test.rb
@@ -8,297 +8,166 @@ require './lib/complete_me'
 require './lib/node'
 
 class CompleteMeTest < Minitest::Test
-  extend Minitest::Spec::DSL
-  # TODO: Consolidate test setup & clean up tests
-  # TODO: Ensure full test coverage
 
-  def test_it_exists
-    completion = CompleteMe.new
-    assert_instance_of CompleteMe, completion
+  def setup
+    @completion = CompleteMe.new
+    dictionary = File.read('./data/dictionary_tiny_sample')
+    @completion.populate(dictionary)
   end
 
-  def test_last_node_of_new_word
-    completion = CompleteMe.new
-    last_node = completion.insert("pizza")
-    assert_equal "pizza", last_node.word
-    assert_equal "a", last_node.value
+  def test_it_exists
+    assert_instance_of CompleteMe, @completion
   end
 
   def test_it_can_detect_if_word_is_present
-    completion = CompleteMe.new
-    completion.insert("pizza")
-    assert completion.include?("pizza")
+    assert @completion.include?("pizza")
   end
 
   def test_detect_if_word_is_absent
-    completion = CompleteMe.new
-    completion.insert("pizzeria")
-    refute completion.include?("pizza")
-    refute completion.include?("4i2364876")
+    refute @completion.include?("4i2364876")
+  end
+
+  def test_can_insert_new_word
+    refute @completion.include?("computer")
+    @completion.insert("computer")
+    assert @completion.include?("computer")
   end
 
   def test_it_can_count_words
-    completion = CompleteMe.new
-    assert_equal 0, completion.count
-    completion.insert("pizza")
-    assert_equal 1, completion.count
-
-    completion.insert("dog")
-    completion.insert("cat")
-    assert_equal 3, completion.count
+    assert_equal 13, @completion.count
+    @completion.insert("computer")
+    assert_equal 14, @completion.count
   end
 
   def test_it_can_list_lexicon
-    completion = CompleteMe.new
-    completion.insert("pizza")
-    completion.insert("pizzeria")
-    completion.insert("dog")
-
-    lexicon = completion.lexicon
+    lexicon = @completion.lexicon
     assert lexicon.include?("pizza")
-    assert lexicon.include?("pizzeria")
-    assert lexicon.include?("dog")
-    refute lexicon.include?("cat")
+    assert_equal 13, lexicon.size
+    refute lexicon.include?("computer")
   end
 
   def test_it_can_suggest_words
-
-    completion = CompleteMe.new
-    completion.insert("pizza")
-    completion.insert("pize")
-    suggestion = completion.suggest("piz")
-
+    suggestion = @completion.suggest("piz")
     assert suggestion.include?("pize")
     assert suggestion.include?("pizza")
   end
 
   def test_it_doesnt_suggest_word_identical_to_snippit
-    completion = CompleteMe.new
-    completion.insert("pizza")
-    completion.insert("pize")
-    suggestion = completion.suggest("pize")
-
+    suggestion = @completion.suggest("pize")
     refute suggestion.include?("pize")
   end
 
   def test_it_deosnt_suggest_unrelated_words
-    completion = CompleteMe.new
-    completion.insert("pizza")
-    completion.insert("pize")
-    completion.insert("pokemon")
-    suggestion = completion.suggest("piz")
+    suggestion = @completion.suggest("piz")
     refute suggestion.include?("pokemon")
   end
 
   def test_blank_snippet_terminal_node_is_base_node
-    completion = CompleteMe.new
-    completion.insert("pizza")
-    nodes = completion.all_nodes_by_value('i')
-    eow_node = completion.terminal_node("", nodes[0])
-
-    assert_equal nodes[0], eow_node
+    nodes = @completion.all_nodes_by_value('i')
+    first_node = nodes[0]
+    last_node = @completion.terminal_node("", first_node)
+    assert_equal first_node, last_node
   end
 
   def test_word_is_unflagged_when_deleted
-    completion = CompleteMe.new
-    completion.insert("pizza")
-    completion.insert("pize")
-    completion.insert("pokemon")
-    assert completion.include?("pokemon")
-    completion.delete("pokemon")
-    refute completion.include?("pokemon")
+    assert @completion.include?("pokemon")
+    @completion.delete("pokemon")
+    refute @completion.include?("pokemon")
   end
 
   def test_trie_trims_unused_nodes_on_deletion
-    completion = CompleteMe.new
-    completion.insert("stud")
-    completion.insert("student")
-    completion.insert("students")
-    completion.insert("studio")
-    term = completion.terminal_node("stud")
+    term = @completion.terminal_node("stud")
     assert_equal 2 ,term.children.size
-
-    completion.delete("student")
-    completion.delete("students")
-    assert completion.include?("stud")
+    @completion.delete("student")
+    @completion.delete("students")
+    assert @completion.include?("stud")
     assert_equal 1 ,term.children.size
-
   end
 
   def test_trie_doesnt_overtrim_on_deletion
-    completion = CompleteMe.new
-    completion.insert("stud")
-    completion.insert("student")
-    completion.insert("students")
-    completion.insert("studio")
-
-    completion.delete("student")
-    assert completion.include?("students")
-    refute completion.include?("student")
+    @completion.delete("student")
+    assert @completion.include?("students")
+    refute @completion.include?("student")
   end
 
   def test_it_can_find_all_nodes_for_specific_character
-    completion = CompleteMe.new
-    completion.insert("pizza")
-    completion.insert("pize")
-    completion.insert("pokemon")
-
-    nodes = completion.all_nodes_by_value('z')
+    nodes = @completion.all_nodes_by_value('z')
     assert_instance_of Node, nodes[0]
     assert_equal 2, nodes.size
-
-    nodes = completion.all_nodes_by_value('e')
-    assert_equal 2, nodes.size
+    nodes = @completion.all_nodes_by_value('e')
+    assert_equal 7, nodes.size
   end
 
-
   def test_absent_snippit_returns_no_suggestions
-    completion = CompleteMe.new
-    completion.insert("pizza")
-    completion.insert("pize")
-    completion.insert("pokemon")
-    suggestion = completion.suggest_all("aaa")
-
+    suggestion = @completion.suggest("aaa")
     assert_equal [], suggestion
   end
 
-  def test_suggests_words_with_midword_snippit
-    completion = CompleteMe.new
-    completion.insert("pizza")
-    completion.insert("pize")
-    completion.insert("pokemon")
-    suggestion = completion.suggest_all("zz")
+  def test_can_suggest_words_with_midword_snippit
+    suggestion = @completion.suggest("zz")
+    assert suggestion.include?("pizza")
+    assert suggestion.include?("pizzeria")
+  end
 
-    assert_equal ['pizza'], suggestion
+  def test_can_suggest_words_with_endword_snippit
+    suggestion = @completion.suggest("mon")
+    assert suggestion.include?("pokemon")
+  end
+
+  def test_can_weight_words_based_on_selction
+    @completion.select("piz", "pizzeria")
+    suggestion = @completion.suggest("piz")
+    assert_equal "pizzeria", suggestion[0]
+  end
+
+  def test_weights_are_independant
+    @completion.select("piz", "pizzeria")
+    @completion.select("pi", "pize")
+    @completion.select("pizz", "pizza")
+    suggestion_1 = @completion.suggest("piz")
+    suggestion_2 = @completion.suggest("pi")
+    suggestion_3 = @completion.suggest("pizz")
+    assert_equal "pizzeria", suggestion_1[0]
+    assert_equal "pize", suggestion_2[0]
+    assert_equal "pizza", suggestion_3[0]
+  end
+
+  def test_can_suggest_addresses
+    suggestion = @completion.suggest("Blvd")
+    assert suggestion.include?("555 N Broadway Blvd")
+  end
+
+  def test_cand_weight_addresses_based_on_selection
+    suggestion = @completion.suggest("Blvd")
+    refute_equal "555 N Broadway Blvd", suggestion[0]
+    @completion.select("Blvd", "555 N Broadway Blvd")
+    suggestion = @completion.suggest("Blvd")
+    assert_equal "555 N Broadway Blvd", suggestion[0]
   end
 
   ########### Tests that load dictionary #############
 
-  def test_can_populate_from_a_dictionary
-    # # skip
+  def test_can_populate_from_full_dictionary_and_suggest_words
     completion = CompleteMe.new
     dictionary = File.read("/usr/share/dict/words")
     completion.populate(dictionary)
-
     assert_equal 235886, completion.count
-    assert completion.include?("tissue")
-  end
-
-  def test_can_suggest_from_dictionary_with_endword_snippit
-    # # skip
-    completion = CompleteMe.new
-    dictionary = File.read("/usr/share/dict/words")
-    completion.populate(dictionary)
-    suggestion = completion.suggest_all("ism")
-    # p suggestion
-    assert suggestion.include?("capitalism")
-  end
-
-  def test_can_suggest_from_dictionary_with_midword_snippit
-    # skip
-    completion = CompleteMe.new
-    dictionary = File.read("/usr/share/dict/words")
-    completion.populate(dictionary)
-    suggestion = completion.suggest_all("mut")
-    assert suggestion.include?("dismutation")
-  end
-
-  def test_can_suggest_many_words_after_population
-    # skip
-    completion = CompleteMe.new
-    dictionary = File.read("/usr/share/dict/words")
-    completion.populate(dictionary)
 
     suggestion = completion.suggest("piz")
-    expected = ["pize", "pizza", "pizzeria", "pizzicato", "pizzle"]
-
-    assert_equal expected, suggestion
-  end
-
-  def test_can_weight_words_based_on_selction
-    # skip
-    completion = CompleteMe.new
-    dictionary = File.read("/usr/share/dict/words")
-    completion.populate(dictionary)
-
-    completion.select("piz", "pizzeria")
-    suggestion = completion.suggest("piz")
-    assert_equal "pizzeria", suggestion[0]
-
-  end
-
-  def test_weights_are_independant
-    # skip
-    completion = CompleteMe.new
-    dictionary = File.read("/usr/share/dict/words")
-    completion.populate(dictionary)
-
-    completion.select("piz", "pizzeria")
-    completion.select("pi", "pillow")
-    completion.select("pizz", "pizza")
-    suggestion_1 = completion.suggest("piz")
-    suggestion_2= completion.suggest("pi")
-    suggestion_3 = completion.suggest("pizz")
-
-    assert_equal "pizzeria", suggestion_1[0]
-    assert_equal "pillow", suggestion_2[0]
-    assert_equal "pizza", suggestion_3[0]
-  end
-
-  def test_it_can_find_character_nodes_from_dictionary
-    # skip
-    completion = CompleteMe.new
-    dictionary = File.read("/usr/share/dict/words")
-    completion.populate(dictionary)
-    nodes = completion.all_nodes_by_value('a', 2)
-    a_node_count = nodes.count do |node|
-      node.value == "a"
-    end
-    assert_equal nodes.size, a_node_count
-    # No good way to make sure all 'a' nodes are returned without
-    # making the test circular.
+    assert suggestion.include?("pizza")
+    assert suggestion.include?("papize")
+    assert suggestion.include?("epizoic")
   end
 
   ########### Tests that load address list #############
 
-  def test_it_can_read_in_addresses
-    # skip
+  def test_can_populate_from_address_list_and_suggest_adresses
     address_completion = CompleteMe.new
     address_completion.populate_addresses("./data/addresses.csv")
-
     assert_equal 313415, address_completion.count
-  end
 
-  def test_it_can_suggest_addresses
-    # skip
-    completion = CompleteMe.new
-    completion.populate_addresses("./data/addresses.csv")
-    suggestion = completion.suggest("1350 N")
-
+    suggestion = address_completion.suggest("1350 N")
     assert suggestion.include?("1350 N Speer Blvd Unit 422")
   end
 
-  def test_it_can_update_address_suggestions_from_selection
-    # skip
-    completion = CompleteMe.new
-    completion.populate_addresses("./data/addresses.csv")
-    suggestion = completion.suggest("1350 N")
-    assert_equal "1350 N Elm St", suggestion[0]
-
-    completion.select("1350 N", "1350 N Josephine St Apt 106")
-    suggestion = completion.suggest("1350 N")
-    assert_equal "1350 N Josephine St Apt 106", suggestion[0]
-  end
-
-  # def test_can_suggest_many_words_after_population
-  #   # skip
-  #   completion = CompleteMe.new
-  #   dictionary = File.read("/usr/share/dict/words")
-  #   completion.populate(dictionary)
-  #
-  #   suggestion = completion.suggest_all("piz")
-  #   # expected = ["pize", "pizza", "pizzeria", "pizzicato", "pizzle"]
-  #   p suggestion
-  #   # assert_equal expected, suggestion
-  # end
 end
